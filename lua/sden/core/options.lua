12280e79db42972e81ae872a98809b82a69f1e5c
Denys Slipetskyy <d.slipetskiy@gmail.com>
2024-04-08 13:37:58 +0300

    WIP manual nvim config

---
+++ lua/sden/core/options.lua
@@ -0,0 +1,42 @@
+vim.cmd("let g:netrw_liststyle = 3")
+
+local opt = vim.opt
+
+-- I want line numbering style for easier command ranging
+opt.relativenumber = true
+opt.number = true
+
+-- I want to share system clipboard
+opt.clipboard = 'unnamedplus'
+
+-- tabs & idents
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- appearance
+
+-- turn on termguicolors for nightfly colorscheme to work
+-- (have to use iterm2 or any other true color terminal)
+opt.termguicolors = true
+opt.background = "dark" -- colorschemes that can be light or dark will be made dark
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
+
+-- turn off swapfile
+opt.swapfile = false
